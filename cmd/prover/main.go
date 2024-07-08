b63f95f3fbf22d5da930897454a9f1105c16be8e
ny4rl4th0t3p <94838432+ny4rl4th0t3p@users.noreply.github.com>
2024-07-08 21:29:18 +0200

    feat(prover): add main functionality in prover module

---
+++ cmd/prover/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"context"
+	"log"
+	"log/slog"
+	"time"
+
+	"practical-case-test/config"
+	"practical-case-test/internal/app"
+	interactor "practical-case-test/internal/interactor/grpc"
+)
+
+// main initializes the client and performs the following actions:
+// 1. Generates a random userName and userPassword.
+// 2. Registers the user with the client using the generated userName and userPassword.
+// 3. Logs in with the registered user credentials.
+// 4. Prints the session ID of the successful login.
+// 5. Sleeps for 60 seconds before ending the program.
+func main() {
+	cfg := config.LoadConfig()
+
+	client, err := interactor.NewClient(
+		cfg.VerifierURL,
+		cfg,
+		app.NewRegister(),
+		app.NewCommitment(),
+		app.NewComputeS(),
+	)
+	if err != nil {
+		log.Fatalf("Failed to create client: %v", err)
+	}
+	defer func(client *interactor.AuthenticationClient) {
+		_ = client.Close()
+	}(client)
+
+	var userName = app.RandString(10)
+
+	userPassword, err := app.RandomPassword()
+	if err != nil {
+		slog.Error("error creating random password", "error", err)
+		return
+	}
+
+	err = client.Register(context.Background(), userName, userPassword)
+	if err != nil {
+		slog.Error("register failed", "error", err)
+		return
+	}
+
+	sessionID, err := client.Login(context.Background(), userName, userPassword)
+	if err != nil {
+		slog.Error("login failed", "error", err)
+		return
+	}
+
+	log.Printf("Successfully logged in with session ID: %s", sessionID)
+
+	slog.Info("Sleeping for 60 seconds before killing prover")
+
+	time.Sleep(60 * time.Second)
+}
